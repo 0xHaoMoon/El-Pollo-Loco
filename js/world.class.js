9dabcf5bc13886dbd3b8a3df467aa2ea7594de0a
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-06-14 21:49:10 +0200

    show character

--- js/world.class.js
+++ js/world.class.js
@@ -1,14 +1,24 @@
 class World {
     character = new Character();
     enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
 
+    ctx;
 
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw(){
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
 
+        for (let i = 0; i < this.enemies.length; i++) {
+            const enemy = this.enemies[i];
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+        }
     }
 }
