43da59a7ebef19e2e5edcdda174f750a5401b314
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-04 16:02:24 +0200

    animate clouds

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -5,12 +5,23 @@ class MovableObject{
     height = 150;
     width = 200;
 
+    imageCache = [];
+
     // loadImage('img/test.png')
     loadImage(path){
         this.img = new Image () // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
 
     }
+
+    loadImages(arr){
+        arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = path;
+        });
+    }
+     
     moveRight(){
         console.log('Moving right')
     };
