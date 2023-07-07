968380dcd86bb47733c5e9a801a3e5db2f999524
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-07 13:15:05 +0200

    add background objects

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -11,10 +11,26 @@ class World {
     ];
 
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -999, 400 ),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -999 ),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -999 ),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -999 ),
+
         new BackgroundObject('img/5_background/layers/air.png', 0, 400 ),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0 ),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0 ),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0 )
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0 ),
+
+        new BackgroundObject('img/5_background/layers/air.png', 999, 400 ),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 999 ),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 999 ),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 999 ),
+
+
+        new BackgroundObject('img/5_background/layers/air.png', 999*2, 400 ),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 999*2, ),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 999*2, ),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 999*2, ),
         
     ];
     canvas;
