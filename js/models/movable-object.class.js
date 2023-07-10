bcb4fd050330cfe9e94fa098b1172af8238689b4
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-10 17:40:27 +0200

    add rect() method

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -32,6 +32,19 @@ class MovableObject{
 
     }
 
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+    }
+
+    drawFrame(ctx){
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
     loadImages(arr){
         arr.forEach((path) => {
         let img = new Image();
