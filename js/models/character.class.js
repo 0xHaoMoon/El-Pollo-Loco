4baf5c04d7d18db020d12ac1b2cb5d7728fd6c0e
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-12 17:09:47 +0200

    add damage & add dead animation

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -25,6 +25,17 @@ class Character extends MovableObject{
         'img/2_character_pepe/3_jump/J-38.png',
         'img/2_character_pepe/3_jump/J-39.png'
     ];
+
+    IMAGES_DEAD = [
+        'img/2_character_pepe/5_dead/D-51.png',
+        'img/2_character_pepe/5_dead/D-52.png',
+        'img/2_character_pepe/5_dead/D-53.png',
+        'img/2_character_pepe/5_dead/D-54.png',
+        'img/2_character_pepe/5_dead/D-55.png',
+        'img/2_character_pepe/5_dead/D-56.png',
+        'img/2_character_pepe/5_dead/D-57.png',
+
+    ];
     world;
     walking_sound = new Audio('audio/walk.mp3');
 
@@ -32,6 +43,7 @@ class Character extends MovableObject{
             super().loadImage('img/2_character_pepe/2_walk/W-21.png');
             this.loadImages(this.IMAGES_WALKLING);
             this.loadImages(this.IMAGES_JUMPING);
+            this.loadImages(this.IMAGES_DEAD);
             this.applyGravity();
             this.animate();
     }
@@ -62,7 +74,11 @@ class Character extends MovableObject{
 
         setInterval(() => {
 
-            if(this.IsAboveGround()){
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            }
+
+            else if(this.IsAboveGround()){
                 this.playAnimation(this.IMAGES_JUMPING);
             } else{
 
